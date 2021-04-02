2ce9dea182f0915e25d2205342fcc9ce6d304bbf
Jose Caicedo <jose@jrc-macbook-new.myfiosgateway.com>
2021-04-02 00:05:35 -0400

    intial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+
+
+window.onload = function() {
+
+    function removeAllChildNodes(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
+
+    // function profilePicture() {
+    //     // let picWrapper = document.querySelector("#pic-wrapper")
+    //     // let profilePic = document.createElement("img");
+    //     // profilePic.src = "selfpic.png"
+    //     // picWrapper.appendChild(profilePic);
+
+    // }
+
+    function resizeContent(){
+
+        let textWrapper = document.querySelector("#circle-text")
+        let picWrapper = document.querySelector("#pic-wrapper")
+
+        removeAllChildNodes(textWrapper)
+        removeAllChildNodes(picWrapper)
+
+        circularText("ENGINEER • DEVELOPER • THINKER • TEACHER • ");
+        profilePicture();
+    }
+
+    function circularText(txt) {
+
+        txt = txt.split("")
+        let textWrapper = document.querySelector("#circle-text")
+
+
+        const wrapperWidth = textWrapper.offsetWidth;
+        const wrapperHeight = textWrapper.offsetHeight;
+
+        const radius =  wrapperWidth < wrapperHeight ? wrapperWidth * 1 : wrapperHeight * 1
+      
+        var deg = 360 / txt.length,
+        origin = 0;
+    
+        txt.forEach((ea) => {
+            ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:center; '>${ea}</p>`;
+            textWrapper.innerHTML += ea;
+            origin += deg;
+        });
+
+        textWrapper.classList.add("rotate")
+
+        let picWrapper = document.querySelector("#pic-wrapper")
+        let profilePic = document.createElement("img");
+        profilePic.src = "selfpic.png"
+        picWrapper.appendChild(profilePic);
+        profilePic.style.height = radius / 1.4 + 'px'
+        profilePic.style.width = 'auto'
+    }
+    
+    circularText("ENGINEER • DEVELOPER • THINKER • TEACHER • ");
+
+    window.addEventListener('resize', resizeContent )
+    
+}
